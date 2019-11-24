2f60de51fd052e20bccbe8bc095c94da6881c766
Ramon Manrique Figueroa <ramon.manfig@gmail.com>
2019-11-24 16:31:54 -0600

    Added Helper File for capturing images

---
+++ app/src/main/java/com/example/qrml/Helper/ImageHelper.java
@@ -0,0 +1,4 @@
+package com.example.qrml.Helper;
+
+public class ImageHelper {
+}
--- app/src/main/java/com/example/qrml/Splash/Splash.java
+++ app/src/main/java/com/example/qrml/Splash/Splash.java
@@ -1,4 +1,28 @@
 package com.example.qrml.Splash;
 
-public class Splash {
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.qrml.MainActivity;
+import com.example.qrml.R;
+
+public class Splash extends AppCompatActivity {
+    private final int DURACION = 3000;
+    //private View img = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_layout);
+        new Handler().postDelayed(new Runnable(){
+            public void run(){
+                Intent intent = new Intent(Splash.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            };
+        }, DURACION);
+    }
 }
