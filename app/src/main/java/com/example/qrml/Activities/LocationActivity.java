9ce7b7fab03a92b572a90aba035c0b8a6111e1bd
MFRamon <ramon.manfig@gmail.com>
2019-11-25 13:14:44 -0600

    Login Activity getting all the data from the User Logging in

---
+++ app/src/main/java/com/example/qrml/Activities/LocationActivity.java
@@ -0,0 +1,16 @@
+package com.example.qrml.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.qrml.R;
+
+public class LocationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_location);
+    }
+}
